0e4d6131138a4af266ee0663cee3b16e0def461d
kavishka <akdaswaththa2001@gmail.com>
2025-07-16 14:51:44 +0530

    CRUD

---
+++ a_core/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.urls import path
+from a_posts.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',home_view, name='home'),
+    path('post/create/', post_create_view, name="post-create"),
+    path('post/delete/<pk>', post_delete_view, name="post-delete"),
+    path('post/edit/<pk>', post_edit_view, name="post-edit"),
+    path('post/<pk>', post_page_view, name="post")
+
+
+]
