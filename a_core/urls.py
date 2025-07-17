67396958868efd249ad0502f7b69bdd7b1f8a3ae
kavishka <akdaswaththa2001@gmail.com>
2025-07-17 14:22:18 +0530

    Image Files handling

--- a_core/urls.py
+++ a_core/urls.py
@@ -2,6 +2,9 @@
 from django.contrib import admin
 from django.urls import path
 from a_posts.views import *
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,3 +19,5 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
