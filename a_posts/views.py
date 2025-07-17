7ff5be2ef4a4437358783037d72d66b34321d242
kavishka <akdaswaththa2001@gmail.com>
2025-07-17 10:54:17 +0530

    Tag implement

--- a_posts/views.py
+++ a_posts/views.py
@@ -11,11 +11,25 @@ def welcome_view (request):
     return render(request,'welcome.html', {'posts':posts})
 
   
-def home_view (request):
-    posts = Post.objects.all()
-    return render(request,'a_posts/home.html', {'posts':posts})
+def home_view (request, tag=None):
+    if tag:
+        posts = Post.objects.filter(tags__slug=tag)
+        tag = get_object_or_404(Tag, slug=tag)
+    else:
+        posts = Post.objects.all()
+        
+    categories = Tag.objects.all()
+    
+    context = {
+        'posts' : posts,
+        'categories' : categories,
+        'tag' : tag
+    }
+    
+    return render(request,'a_posts/home.html', context)
 
-  
+
+
 
 def post_create_view(request):
     form = PostCreateForm()
@@ -41,6 +55,7 @@ def post_create_view(request):
             post.artist = artist
             
             post.save()
+            form.save_m2m()
             return redirect('home')
         
     return render(request, 'a_posts/post_create.html', {'form': form})
