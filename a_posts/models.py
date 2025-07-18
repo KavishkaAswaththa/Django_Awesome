67236f56fa71234b66d9a71013f35d64f8371ccf
kavishka <akdaswaththa2001@gmail.com>
2025-07-19 02:43:46 +0530

    comment & replies

--- a_posts/models.py
+++ a_posts/models.py
@@ -6,7 +6,7 @@ import uuid
 class Post(models.Model):
     title = models.CharField(max_length=500)
     artist = models.CharField(max_length=500, null=True)
-    url = models.URLField(max_length=500, null=True)
+    url = models.URLField(max_length=1000, null=True)
     image = models.URLField(max_length=500)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='posts')
     body = models.TextField()
@@ -33,3 +33,40 @@ class Tag(models.Model):
     
     class Meta:
         ordering = ['order']
+        
+        
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='comments')
+    parent_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    body = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        try:
+            return f'{self.author.username} : {self.body[:30]}'
+        except:
+            return f'no author : {self.body[:30]}'
+        
+    class Meta:
+        ordering = ['-created']
+        
+        
+        
+class Reply(models.Model):
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='replies')
+    parent_comment = models.ForeignKey(Comment, on_delete=models.CASCADE, related_name='replies')
+    body = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.CharField(max_length=100, default=uuid.uuid4, unique=True, primary_key=True, editable=False)
+    
+    def __str__(self):
+        try:
+            return f'{self.author.username} : {self.body[:30]}'
+        except:
+            return f'no author : {self.body[:30]}'
+        
+    class Meta:
+        ordering = ['-created']
